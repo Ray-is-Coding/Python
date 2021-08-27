1b58a4db3a439152981da802833c513a5dca13f2
Ray-is-Coding <89632133+Ray-is-Coding@users.noreply.github.com>
2021-08-27 18:31:13 +0530

    Update ReverseIntwith32bitConstraint.py

--- ReverseIntwith32bitConstraint.py
+++ ReverseIntwith32bitConstraint.py
@@ -1,3 +1,4 @@
+Solution 1
 class Solution:
     def reverse(self, x: int) -> int:
         if x >= pow(-2,31) and x <= (pow(2,31)+1):
@@ -18,9 +19,38 @@ class Solution:
         else:
             return 0
        
+Solution 2         
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x >= pow(-2,31) and x <= (pow(2,31)+1):
+            r = 0
+            s = abs(x)
+            while s > 0:
+                d =  s % 10
+                r = r * 10 + d
+                s //= 10
+
+            if r >= pow(-2,31) and r <= (pow(2,31)+1):
+                if x > 0:
+                    r = r
+                else:
+                    r = -r
+            else:
+                return 0
+        else:
+            return 0
+
+        if r == x and x > 0:
+            return True
+        elif r == x and x < 0:
+            return False
+        elif x == 0:
+            return True
+       
         
-                
-            
+        
+        
+        
             
             
             
