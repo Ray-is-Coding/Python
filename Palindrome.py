f430ecfe65e22197628400a810ad66202efd0d38
Ray-is-Coding <89632133+Ray-is-Coding@users.noreply.github.com>
2021-08-27 18:32:19 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,3 +1,4 @@
+Solution 1
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         a = abs(x)
@@ -10,3 +11,30 @@ class Solution:
         elif x == 0: 
             return True
         
+Solution 2         
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x >= pow(-2,31) and x <= (pow(2,31)+1):
+            r = 0
+            s = abs(x)
+            while s > 0:
+                d =  s % 10
+                r = r * 10 + d
+                s //= 10
+
+            if r >= pow(-2,31) and r <= (pow(2,31)+1):
+                if x > 0:
+                    r = r
+                else:
+                    r = -r
+            else:
+                return 0
+        else:
+            return 0
+
+        if r == x and x > 0:
+            return True
+        elif r == x and x < 0:
+            return False
+        elif x == 0:
+            return True
